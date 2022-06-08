ae6c07c205561d3460f67f83648c69d974a99e04
NidhishaPahade <101486006+NidhishaPahade@users.noreply.github.com>
2022-06-09 05:50:32 +1000

    Add files via upload

---
+++ frontend/src/api/employeeApi.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { baseApiUrl } from "../config/config";
+
+const employeeApiUrl = baseApiUrl + "employee/";
+
+class EmployeeApi {
+  static getEmployees() {
+    return axios({
+      method: "get",
+      url: employeeApiUrl + "getEmployeeList"
+    });
+  }
+
+  static updateEmployee(employee, id) {
+    return axios({
+      method: "post",
+      url: employeeApiUrl + "updateEmployee/" + id,
+      data: {
+        employee,
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  
+
+  static getEmployee(value) {
+    return axios({
+      method: "get",
+      url: `${employeeApiUrl}getEmployee?employeeSheetName=${value}`
+    });
+  }
+
+  static addEmployee(employee, lastRowNumber) {
+    return axios({
+      method: "post",
+      url: employeeApiUrl + "addEmployee",
+      data: {
+        employee,
+        lastRowNumber
+      },
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  static deleteEmployee(rowNumber, id) {
+    return axios({
+      method: "delete",
+      url: employeeApiUrl + "deleteEmployee/" + id,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+}
+
+export default EmployeeApi;
+
