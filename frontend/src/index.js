c16e2177dfa3d75f3821e3a4e12e47fdba32d343
NidhishaPahade <101486006+NidhishaPahade@users.noreply.github.com>
2022-06-09 05:51:51 +1000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+
 import ReactDOM from "react-dom";
+import React from "react";
 import App from "./components/App";
 // import registerServiceWorker from "./registerServiceWorker";
 import "react-toastify/dist/ReactToastify.css";
